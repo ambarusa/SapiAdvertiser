55e5573e79af3ff16fb5dcab7df8c04a6c3abceb
laszlosomai <somailaszlo@gmail.com>
2018-11-20 16:56:11 +0200

    Added splash screen
    Login/Register activity befejezve
    A splash screen-en az icont kell meretezni

--- app/src/main/java/com/example/lszlsomai/sapiadvertiser/LoginActivity.java
+++ app/src/main/java/com/example/lszlsomai/sapiadvertiser/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.lszlsomai.sapiadvertiser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToRegistration(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
